150825091922515013d214aab87d63b508ded5cd
Srijith <srijithreddy.pingili@cerner.com>
2020-08-18 17:23:01 -0500

    Making Our Service a Foreground Service

--- app/src/main/java/sp/android/fitnesstracker/play/services/TrackingService.kt
+++ app/src/main/java/sp/android/fitnesstracker/play/services/TrackingService.kt
@@ -1,21 +1,38 @@
 package sp.android.fitnesstracker.play.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import sp.android.fitnesstracker.play.R
+import sp.android.fitnesstracker.play.ui.MainActivity
+import sp.android.fitnesstracker.play.util.Constants
 import sp.android.fitnesstracker.play.util.Constants.ACTION_PAUSE_SERVICE
 import sp.android.fitnesstracker.play.util.Constants.ACTION_START_OR_RESUME_SERVICE
 import sp.android.fitnesstracker.play.util.Constants.ACTION_STOP_SERVICE
+import sp.android.fitnesstracker.play.util.Constants.NOTIFICATION_CHANNEL_ID
+import sp.android.fitnesstracker.play.util.Constants.NOTIFICATION_ID
 import timber.log.Timber
 
 class TrackingService : LifecycleService() {
-
+    var isFirstRun = true
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         intent?.let {
             when (it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    Timber.d("Started or resumed service")
+                    if (isFirstRun) {
+                        startForegroundService()
+                    } else {
+                        Timber.d("Resuming service")
+                    }
+
                 }
                 ACTION_PAUSE_SERVICE -> {
                     Timber.d("Paused service")
@@ -28,5 +45,46 @@ class TrackingService : LifecycleService() {
 
 
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForegroundService() {
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(
+            Constants.NOTIFICATION_CHANNEL_ID,
+            Constants.NOTIFICATION_CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
     }
 }
