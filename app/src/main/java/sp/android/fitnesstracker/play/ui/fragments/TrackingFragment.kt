6bfd991f32646fbf7d2205affb783375e82fbab7
Srijith <srijithreddy.pingili@cerner.com>
2020-08-18 16:02:25 -0500

    map lifecycle

--- app/src/main/java/sp/android/fitnesstracker/play/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/sp/android/fitnesstracker/play/ui/fragments/TrackingFragment.kt
@@ -1,8 +1,12 @@
 package sp.android.fitnesstracker.play.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 import sp.android.fitnesstracker.play.R
 import sp.android.fitnesstracker.play.ui.viewmodels.MainViewModel
 
@@ -10,4 +14,49 @@ import sp.android.fitnesstracker.play.ui.viewmodels.MainViewModel
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    //looks like onDestroy() might not be needed, mapview gets
+    // destroyed before??
 }
