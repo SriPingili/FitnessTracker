1215af49849452f81a2e5e25b7c709b1daebe730
Srijith <srijithreddy.pingili@cerner.com>
2020-08-18 14:32:28 -0500

    handled location permissions - easyreader library

--- app/src/main/java/sp/android/fitnesstracker/play/ui/fragments/RunFragment.kt
+++ app/src/main/java/sp/android/fitnesstracker/play/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package sp.android.fitnesstracker.play.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,11 +9,15 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 import sp.android.fitnesstracker.play.R
 import sp.android.fitnesstracker.play.ui.viewmodels.MainViewModel
+import sp.android.fitnesstracker.play.util.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import sp.android.fitnesstracker.play.util.TrackingUtility
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -21,5 +27,55 @@ class RunFragment : Fragment(R.layout.fragment_run) {
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+
+        requestPermissions()
+    }
+
+    fun requestPermissions() {
+
+        if (TrackingUtility.hasLocationPermissions(requireContext())) {
+            return
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
--- app/src/main/java/sp/android/fitnesstracker/play/util/Constants.kt
+++ app/src/main/java/sp/android/fitnesstracker/play/util/Constants.kt
@@ -3,4 +3,5 @@ package sp.android.fitnesstracker.play.util
 object Constants {
 
     const val RUNNING_DATABASE_NAME = "running_database"
+    const val REQUEST_CODE_LOCATION_PERMISSION = 100
 }
