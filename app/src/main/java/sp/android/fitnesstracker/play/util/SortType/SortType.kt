c5f1e0ae1450c2ad1cd022012466caa5ef1a091c
Srijith <srijithreddy.pingili@cerner.com>
2020-08-22 06:46:07 -0500

    Sorting Runs

---
+++ app/src/main/java/sp/android/fitnesstracker/play/util/SortType/SortType.kt
@@ -0,0 +1,9 @@
+package sp.android.fitnesstracker.play.util.SortType
+
+/**
+ * Describes the sort type of the run list
+ */
+enum class SortType {
+    DATE, RUNNING_TIME, DISTANCE, AVG_SPEED, CALORIES_BURNED
+}
+
