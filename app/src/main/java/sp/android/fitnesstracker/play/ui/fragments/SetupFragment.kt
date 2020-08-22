b895bf47689615b6e4bb55c7f965a4ef80c796eb
Srijith <srijithreddy.pingili@cerner.com>
2020-08-22 07:22:15 -0500

    Saving Username and Weight

--- app/src/main/java/sp/android/fitnesstracker/play/ui/fragments/SetupFragment.kt
+++ app/src/main/java/sp/android/fitnesstracker/play/ui/fragments/SetupFragment.kt
@@ -1,25 +1,74 @@
 package sp.android.fitnesstracker.play.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_setup.*
 import sp.android.fitnesstracker.play.R
 import sp.android.fitnesstracker.play.ui.viewmodels.MainViewModel
+import sp.android.fitnesstracker.play.util.Constants.KEY_FIRST_TIME_TOGGLE
+import sp.android.fitnesstracker.play.util.Constants.KEY_NAME
+import sp.android.fitnesstracker.play.util.Constants.KEY_WEIGHT
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+    @Inject
+    lateinit var sharedPref: SharedPreferences
 
-    private val viewModel: MainViewModel by viewModels()
+    @set:Inject
+    var firstTimeAppOpen: Boolean = true
+    @set:Inject
+    var name: String = "Amigos"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tvContinue.setOnClickListener {
-            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        if (!firstTimeAppOpen) {
+            val toolbarText = "Let's go, $name!"
+            requireActivity().tvToolbarTitle.text = toolbarText
+
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.setupFragment, true)
+                .build()
+            findNavController().navigate(
+                R.id.action_setupFragment_to_runFragment,
+                savedInstanceState,
+                navOptions
+            )
         }
+
+        tvContinue.setOnClickListener {
+            val success = writePersonalDataToSharedPref()
+            if (success) {
+                findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+            } else {
+                Snackbar.make(requireView(), "Please enter all the fields.", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun writePersonalDataToSharedPref(): Boolean {
+        val name = etName.text.toString()
+        val weightText = etWeight.text.toString()
+        if (name.isEmpty() || weightText.isEmpty()) {
+            return false
+        }
+        sharedPref.edit()
+            .putString(KEY_NAME, name)
+            .putFloat(KEY_WEIGHT, weightText.toFloat())
+            .putBoolean(KEY_FIRST_TIME_TOGGLE, false)
+            .apply()
+        val toolbarText = "Let's go, $name!"
+        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
     }
 }
